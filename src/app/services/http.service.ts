21b368efa9c11dc420eab5317c1e811e2baf9294
zhenglingfeng <bszhenglf@163.com>
2017-07-26 15:04:32 +0800

    前端增加接口回调统一处理

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -30,7 +30,9 @@ export class HttpService {
     post(url, params = {}, contentType = 'application/json') {
         const headers = new Headers({ 'Content-Type': contentType, 'Authorization': this.getToken() });
         const options = new RequestOptions({ headers });
-        return this.http.post(url, JSON.stringify(params), options);
+        return this.http.post(url, JSON.stringify(params), options)
+            .map(this.extractData)
+            .catch(this.handleError);;
     }
 
     get(url, params = {}, contentType = 'application/json') {
@@ -40,10 +42,6 @@ export class HttpService {
         url = url + queryString;
         return this.http.get(url, options);
     }
-
-     private extractData(res: Response) {
-         return res.json()._body;
-     }
 
     handleGetParams(params): string {
         let queryString = '';
@@ -55,6 +53,48 @@ export class HttpService {
         const exp = /^&/;
         queryString = queryString.length > 0 ? queryString.replace(exp, '?') : '';
         return queryString;
+    }
+
+    handleFormParams(params): string {
+        let queryString = '';
+        for (let i in params) {
+            if (params[i] != null) {
+                queryString += '&' + i + '=' + params[i];
+            }
+        }
+        return queryString;
+    }
+
+    // 处理接口回调
+    private extractData(res: Response) {
+        // let body = res.json();
+        // if (body.code === 200) {
+        //     return (body.data === false) ? false : (body.data || true);
+        // }
+        // else if (body.code === 403) {
+        //     location.href = this.getLoginUrl();
+        // } else {
+        //     COMMON.toast(body.desc, false); //弹框样式,首页内置
+        //     return false;
+        // }
+        return res.json()._body;
+    }
+    // 处理回调异常
+    handleError(error: Response | any) {
+        if (error.status && error.status === 403) {
+            if (localStorage['login_url']) {
+                location.href = localStorage['login_url'];
+            }
+        }
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        return Observable.throw(errMsg);
     }
 }
 
